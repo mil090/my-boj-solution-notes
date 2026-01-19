e9b83497909f9c5bcbddf5f6596d1c421a33e9ba
mil090 <mil09040@gmail.com>
2026-01-20 01:29:41 +0900

    9506번: 약수들의 합 해결

--- numbers1.py
+++ numbers1.py
@@ -48,6 +48,7 @@ while True:
             print('factor')
         else:
             print('neither')
+
 # 2501번: 약수 구하기
 # 문제
 '''
@@ -91,3 +92,54 @@ try:
     print(factors[K-1])
 except:
     print(0)
+
+# 9506번: 약수들의 합
+# 문제
+'''
+어떤 숫자 n이 자신을 제외한 모든 약수들의 합과 같으면, 그 수를 완전수라고 한다.
+예를 들어 6은 6 = 1 + 2 + 3 으로 완전수이다.
+n이 완전수인지 아닌지 판단해주는 프로그램을 작성하라.
+'''
+# 입력
+'''
+입력은 테스트 케이스마다 한 줄 간격으로 n이 주어진다. (2 < n < 100,000)
+입력의 마지막엔 -1이 주어진다.
+'''
+# 출력
+'''
+테스트케이스 마다 한줄에 하나씩 출력해야 한다.
+n이 완전수라면, n을 n이 아닌 약수들의 합으로 나타내어 출력한다(예제 출력 참고).
+이때, 약수들은 오름차순으로 나열해야 한다.
+n이 완전수가 아니라면 n is NOT perfect. 를 출력한다.
+'''
+# 해법
+'''
+1. while True로 무한 루프 틀을 생성
+2. n을 입력받음. 만약 입력받은 n이 -1이면 break로 반복문을 탈출
+3. n 자신을 제외한 n의 약수들을 저장할 빈 리스트 factors를 생성
+4. 1부터 n-1까지의 자연수 i에 대하여 i가 n의 약수이면, 즉 n을 i로 나눈 나머지가
+0이면 i를 factors에 삽입
+5. factors의 모든 원소의 합과 n을 비교
+6-1. 두 값이 같으면 결과로 출력할 빈 문자열 result를 생성. result에 'n =' 를 추가
+factors의 각 원소 f에 대하여 result에 f를 추가. 만약 f가 factors의 최댓값이
+아니라면 result에 ' + '를 추가. 반복문이 종료되면 result를 출력
+6-2. 두 값이 다르면 'n is NOT perfect'를 출력
+'''
+while True:
+    n=int(input())
+    if n==-1:
+        break
+    factors=[]
+    for i in range(1, n):
+        if n%i==0:
+            factors.append(i)
+    if n==sum(factors):
+        result=''
+        result+=f'{n} = '
+        for f in factors:
+            result+=f'{f}'
+            if f!=max(factors):
+                result+=' + '
+        print(result)
+    else:
+        print(f'{n} is NOT perfect.')
