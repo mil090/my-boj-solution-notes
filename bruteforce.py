4d0bab6b4165509c21f1b236c70c35b3a9bafcab
mil090 <mil09040@gmail.com>
2026-01-22 23:07:41 +0900

    2798번: 블랙잭 해결

--- bruteforce.py
+++ bruteforce.py
@@ -1,1 +1,54 @@
 # 브루트 포스
+# 2798번: 블랙잭
+# 문제
+'''
+카지노에서 제일 인기 있는 게임 블랙잭의 규칙은 상당히 쉽다. 카드의 합이 21을 넘지 않는 
+한도 내에서, 카드의 합을 최대한 크게 만드는 게임이다. 블랙잭은 카지노마다 다양한 규정이 
+있다.
+한국 최고의 블랙잭 고수 김정인은 새로운 블랙잭 규칙을 만들어 상근, 창영이와 게임하려고 한다.
+김정인 버전의 블랙잭에서 각 카드에는 양의 정수가 쓰여 있다. 그 다음, 딜러는 N장의 카드를 
+모두 숫자가 보이도록 바닥에 놓는다. 그런 후에 딜러는 숫자 M을 크게 외친다.
+이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다. 블랙잭 
+변형 게임이기 때문에, 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 
+만들어야 한다.
+N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 
+3장의 합을 구해 출력하시오.
+'''
+# 입력
+'''
+첫째 줄에 카드의 개수 N(3 ≤ N ≤ 100)과 M(10 ≤ M ≤ 300,000)이 주어진다. 
+둘째 줄에는 카드에 쓰여 있는 수가 주어지며, 이 값은 100,000을 넘지 않는 양의 정수이다.
+합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우만 입력으로 주어진다.
+'''
+# 출력
+'''
+첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.
+'''
+# 해법
+'''
+1. N, M을 입력받음
+2. N개의 숫자를 입력받아 리스트 numbers에 저장
+3. numbers에서 뽑은 세 숫자의 합을 구해 저장하기 위한 빈 리스트 result를 생성
+4. itertools 모듈의 combinations 메소드를 이용하여 numbers에서 서로 다른 세 
+숫자를 뽑고, 그 합을 result에 저장
+5. result의 최댓값을 출력
+'''
+from itertools import combinations
+N, M=map(int, input().split())
+numbers=list(map(int, input().split()))
+result=[]
+for com in combinations(numbers, 3):
+    if sum(com)<=M:
+        result.append(sum(com))
+print(max(result))
+# 연산 시간을 줄이기 위해 input 대신 sys.stdin.readline을 이용하면?
+from sys import stdin
+from itertools import combinations
+N, M=map(int, stdin.readline().split())
+numbers=list(map(int, stdin.readline().split()))
+result=[]
+coms=list(combinations(numbers, 3))
+for com in coms:
+    if sum(com)<=M:
+        result.append(sum(com))
+print(max(result))
