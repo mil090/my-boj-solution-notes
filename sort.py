3818c4d213d61865f93bbc6f55ba32086ff0f46e
mil090 <mil09040@gmail.com>
2026-01-27 19:23:40 +0900

    11651번: 좌표 정렬하기 2 해결

--- sort.py
+++ sort.py
@@ -361,3 +361,36 @@ for _ in range(N):
 coordinates.sort()
 for coor in coordinates:
     print(coor[0], coor[1])
+
+# 11651번: 좌표 정렬하기 2
+# 문제
+'''
+2차원 평면 위의 점 N개가 주어진다. 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 
+x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+'''
+# 입력
+'''
+첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 
+i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 
+정수이고, 위치가 같은 두 점은 없다.
+'''
+# 출력
+'''
+첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+'''
+# 해법
+'''
+1. N을 입력받음. 좌표를 저장할 빈 리스트 coordinates를 생성
+2. N개의 줄에 걸쳐 좌표를 입력받고, 이를 coordinates에 저장
+3. 람다 함수를 이용하여 각 좌표들을 y좌표, x좌표를 기준으로 정렬
+4. 정렬 결과를 출력
+'''
+import sys
+N=int(sys.stdin.readline())
+coordinates=[]
+for _ in range(N):
+    c=list(map(int, sys.stdin.readline().split()))
+    coordinates.append(c)
+coordinates.sort(key=lambda x: (x[1], x[0]))
+for coor in coordinates:
+    print(coor[0], coor[1])
