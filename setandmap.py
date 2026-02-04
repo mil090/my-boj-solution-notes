5d8eed64e9a8b8f39c1c247c46005407cdd43905
mil090 <mil09040@gmail.com>
2026-02-05 00:15:52 +0900

    11478번: 서로 다른 부분 문자열의 개수 해결

--- setandmap.py
+++ setandmap.py
@@ -450,3 +450,35 @@ N, M=map(int, sys.stdin.readline().split())
 A=set(sys.stdin.readline().split())
 B=set(sys.stdin.readline().split())
 print(len(A.symmetric_difference(B)))
+
+# 11478번: 서로 다른 부분 문자열의 개수
+# 문제
+'''
+문자열 S가 주어졌을 때, S의 서로 다른 부분 문자열의 개수를 구하는 프로그램을 작성하시오.
+부분 문자열은 S에서 연속된 일부분을 말하며, 길이가 1보다 크거나 같아야 한다.
+예를 들어, ababc의 부분 문자열은 a, b, a, b, c, ab, ba, ab, bc, aba, bab, 
+abc, abab, babc, ababc가 있고, 서로 다른것의 개수는 12개이다.
+'''
+# 입력
+'''
+첫째 줄에 문자열 S가 주어진다. S는 알파벳 소문자로만 이루어져 있고, 길이는 1,000 이하이다.
+'''
+# 출력
+'''
+첫째 줄에 S의 서로 다른 부분 문자열의 개수를 출력한다.
+'''
+# 해법
+'''
+1. S를 입력받음. 부분 문자열을 저장할 빈 집합 sub을 생성
+2. S의 길이를 l로 저장
+3. 가능한 부분 문자열의 길이는 1부터 l까지
+4. 각 부분 문자열을 sub에 삽입
+5. sub의 길이를 출력
+'''
+S=input()
+sub=set()
+l=len(S)
+for length in range(1, l+1):
+    for i in range(l-length+1):
+        sub.add(S[i:i+length])
+print(len(sub))
