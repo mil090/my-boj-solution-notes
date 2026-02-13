10d1481563eb4dfa63fb819432c55d66505c1d3e
mil090 <mil09040@gmail.com>
2026-02-13 14:19:33 +0900

    25192번: 인사성 밝은 곰곰이 해결

--- advanced2.py
+++ advanced2.py
@@ -24,3 +24,52 @@
 count=int(input())
 factors=list(map(int, input().split()))
 print(max(factors)*min(factors))
+
+# 25192번: 인사성 밝은 곰곰이
+# 문제
+'''
+알고리즘 입문방 오픈 채팅방에서는 새로운 분들이 입장을 할 때마다 곰곰티콘을 사용해 인사를 
+한다. 이를 본 문자열 킬러 임스는 채팅방의 기록을 수집해 그 중 곰곰티콘이 사용된 횟수를 
+구해 보기로 했다.
+ENTER는 새로운 사람이 채팅방에 입장했음을 나타낸다. 그 외는 채팅을 입력한 유저의 
+닉네임을 나타낸다. 닉네임은 숫자 또는 영문 대소문자로 구성되어 있다.
+새로운 사람이 입장한 이후 처음 채팅을 입력하는 사람은 반드시 곰곰티콘으로 인사를 한다. 
+그 외의 기록은 곰곰티콘을 쓰지 않은 평범한 채팅 기록이다.
+채팅 기록 중 곰곰티콘이 사용된 횟수를 구해보자!
+'''
+# 입력
+'''
+첫 번째 줄에는 채팅방의 기록 수를 나타내는 정수 N이 주어진다. (1<=N<=100,000)
+두 번째 줄부터 N개의 줄에 걸쳐 새로운 사람의 입장을 나타내는 ENTER, 혹은 채팅을 
+입력한 유저의 닉네임이 문자열로 주어진다. (1<={문자열 길이}<=20)
+첫 번째 주어지는 문자열은 무조건 ENTER이다.
+'''
+# 출력
+'''
+채팅 기록 중 곰곰티콘이 사용된 횟수를 출력하시오.
+'''
+# 해법
+'''
+1. N을 입력받음. 채팅을 입력한 사람의 이름을 저장할 빈 집합 names를 생성하고, 결과로
+반환할 변수 result를 생성하여 0으로 초기화
+2. N개의 줄에 걸쳐 ENTER 또는 사람의 이름 chat을 입력받음
+3. chat이 ENTER일 경우, names의 모든 원소를 삭제
+4. chat이 사람 이름일 경우, 기존 names의 길이를 n으로 저장한 다음 chat을 names에
+추가. 새로운 names의 길이가 n보다 클 경우 그 사람은 처음으로 채팅을 친 것이므로
+result에 1을 추가
+5. result를 출력
+'''
+import sys
+N=int(sys.stdin.readline())
+names=set()
+result=0
+for _ in range(N):
+    chat=sys.stdin.readline().strip()
+    if chat=='ENTER':
+        names.clear()
+    else:
+        n=len(names)
+        names.add(chat)
+        if len(names)>n:
+            result+=1
+print(result)
