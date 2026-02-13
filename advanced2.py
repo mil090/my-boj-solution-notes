5f96bc33a28d24d6f39f72f35bd0e96e04303059
mil090 <mil09040@gmail.com>
2026-02-13 20:13:43 +0900

    20920번: 영단어 암기는 괴로워 해결

--- advanced2.py
+++ advanced2.py
@@ -186,3 +186,56 @@ else:
 R=x[-1]-x[0]
 result=list(map(str, [int(traditional_round(avg)), median, mode, R]))
 print('\n'.join(result))
+
+# 20920번: 영단어 암기는 괴로워
+# 문제
+'''
+화은이는 이번 영어 시험에서 틀린 문제를 바탕으로 영어 단어 암기를 하려고 한다. 그 
+과정에서 효율적으로 영어 단어를 외우기 위해 영어 단어장을 만들려 하고 있다. 화은이가 
+만들고자 하는 단어장의 단어 순서는 다음과 같은 우선순위를 차례로 적용하여 만들어진다.
+자주 나오는 단어일수록 앞에 배치한다.
+해당 단어의 길이가 길수록 앞에 배치한다.
+알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다.
+M보다 짧은 길이의 단어의 경우 읽는 것만으로도 외울 수 있기 때문에 길이가 M 이상인 
+단어들만 외운다고 한다. 화은이가 괴로운 영단어 암기를 효율적으로 할 수 있도록 단어장을 
+만들어 주자.
+'''
+# 입력
+'''
+첫째 줄에는 영어 지문에 나오는 단어의 개수 N과 외울 단어의 길이 기준이 되는 M이 공백으로 
+구분되어 주어진다. (1<=N<=100,000, 1<=M<=10)
+둘째 줄부터 N+1번째 줄까지 외울 단어를 입력받는다. 이때의 입력은 알파벳 소문자로만 주어지며 
+단어의 길이는 10을 넘지 않는다.
+단어장에 단어가 반드시 1개 이상 존재하는 입력만 주어진다.
+'''
+# 출력
+'''
+화은이의 단어장에 들어 있는 단어를 단어장의 앞에 위치한 단어부터 한 줄에 한 단어씩 
+순서대로 출력한다.
+'''
+# 해법
+'''
+1. N, M을 입력받음. 외울 단어들을 저장할 빈 덱 words를 생성
+2. N개의 줄에 걸쳐 단어 word를 입력받음
+3. words의 길이가 M 이상일 때만 words에 word를 삽입
+4. Counter를 이용하여 각 단어의 빈도를 구하여 딕셔너리 freq으로 저장
+5. 각 단어들, 즉 freq의 key들을 리스트 result로 저장
+6. result를 사전순으로 정렬(sort())
+7. result를 길이가 긴 순으로 정렬(sort(key=len, reverse=True))
+8. result를 빈도 수가 많은 순서대로 정렬
+9. result의 모든 원소들을 한 줄 간격으로 출력
+'''
+import sys
+from collections import deque, Counter
+N, M=map(int, sys.stdin.readline().split())
+words=deque()
+for _ in range(N):
+    word=sys.stdin.readline().strip()
+    if len(word)>=M:
+        words.append(word)
+freq=dict(Counter(words))
+result=list(freq.keys())
+result.sort()
+result.sort(key=len, reverse=True)
+result.sort(key=lambda x: freq[x], reverse=True)
+print('\n'.join(result))
