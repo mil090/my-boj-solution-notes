a9243f6a33f6c58ef805b542e6b8d690609dbc4d
mil090 <mil09040@gmail.com>
2026-02-16 14:31:47 +0900

    4779번: 칸토어 집합 해결

--- recursion.py
+++ recursion.py
@@ -242,3 +242,107 @@ L=list(map(int, input().split()))
 result=merge_sort(L, 0, len(L)-1, K)
 print(answer)
 # 병합 정렬은 나올 때마다 어려워하므로 반복된 학습이 필요하다!
+
+# 4779번: 칸토어 집합
+# 문제
+'''
+칸토어 집합은 0과 1사이의 실수로 이루어진 집합으로, 구간 [0, 1]에서 시작해서 각 구간을 
+3등분하여 가운데 구간을 반복적으로 제외하는 방식으로 만든다.
+전체 집합이 유한이라고 가정하고, 다음과 같은 과정을 통해서 칸토어 집합의 근사를 만들어보자.
+1. -가 3N개 있는 문자열에서 시작한다.
+2. 문자열을 3등분 한 뒤, 가운데 문자열을 공백으로 바꾼다. 이렇게 하면, 선(문자열) 
+2개가 남는다.
+3. 이제 각 선(문자열)을 3등분 하고, 가운데 문자열을 공백으로 바꾼다. 이 과정은 모든 
+선의 길이가 1일때 까지 계속 한다.
+예를 들어, N=3인 경우, 길이가 27인 문자열로 시작한다.
+---------------------------
+여기서 가운데 문자열을 공백으로 바꾼다.
+남은 두 선의 가운데 문자열을 공백으로 바꾼다.
+---   ---         ---   ---
+한번 더
+- -   - -         - -   - -
+모든 선의 길이가 1이면 멈춘다. N이 주어졌을 때, 마지막 과정이 끝난 후 결과를 출력하는 
+프로그램을 작성하시오.
+'''
+# 입력
+'''
+입력을 여러 줄로 이루어져 있다. 각 줄에 N이 주어진다. 파일의 끝에서 입력을 멈춘다. 
+N은 0보다 크거나 같고, 12보다 작거나 같은 정수이다.
+'''
+# 출력
+'''
+입력으로 주어진 N에 대해서, 해당하는 칸토어 집합의 근사를 출력한다.
+'''
+# 해법
+'''
+1. N을 입력받음
+2. 3**N개의 -로 이루어진 문자열을 토큰화하여 리스트 L로 저장
+3. 칸토어 집합의 근사를 구하는 함수 cantor(l)을 구현
+l의 길이가 1이면 그대로 l을 반환
+l을 같은 길이로 3등분하여 각각을 left, mid, right로 저장
+mid의 모든 원소를 공백으로 대체
+left와 right에 대하여 cantor 함수를 실행
+l을 left+mid+right로 대체하고, l을 반환
+4. cantor(l)을 출력
+'''
+import sys
+def cantor(l: list):
+    n=len(l)
+    if n==1:
+        return l
+    p=n//3
+    q=n//3*2
+    left=l[:p]
+    mid=l[p:q]
+    right=l[q:]
+    mid=[' ']*len(mid)
+    left=cantor(left)
+    right=cantor(right)
+    l=left+mid+right
+    return l
+while True:
+    try:
+        N=int(sys.stdin.readline())
+        L=list('-'*3**N)
+        print(''.join(cantor(L)))
+    except EOFError:
+        break
+# ValueError 발생
+# 주의사항
+'''
+sys.stdin.readline은 입력받은 내용이 없을 경우 빈 문자열을 반환
+따라서 ''을 정수형으로 반환하지 못해 ValueError가 발생한 것
+'''
+for line in sys.stdin:
+    N=int(line)
+    S='-'*3**N
+    L=list(S)
+    print(''.join(cantor(L)))
+# Gemini의 조언: 굳이 문자열을 리스트로 분할할 필요 없이 곧바로 문자열을 바꿔 보자
+'''
+칸토르 집합의 규칙을 파악
+N=0이면 -
+N=1이면 - -
+N=2이면 - -   - -
+N=3이면 - -   - -         - -   - -
+-> N=1의 결과는 공백 1개가 가운데, 그 양 옆에 N=0의 결과
+-> N=2의 결과는 공백 3개가 가운데, 그 양 옆에 N=1의 결과
+-> N=3의 결과는 공백 9개가 가운데, 그 양 옆에 N=2의 결과
+-> N=n의 결과는 공백 3**(n-1)개가 가운데, 그 양 옆에 N=n-1의 결과
+cantor(n)을 새로 정의해 보자
+n이 0이면 -의 개수가 1이므로 -을 반환
+이전 단계의 결과를 prev로 저장
+prev+' '*3**(n-1)+prev를 반환
+'''
+import sys
+from collections import deque
+def cantor(n: int):
+    if n==0:
+        return '-'
+    prev=cantor(n-1)
+    return prev+' '*3**(n-1)+prev
+result=deque()
+for line in sys.stdin:
+    N=int(line)
+    result.append(cantor(N))
+print('\n'.join(result))
